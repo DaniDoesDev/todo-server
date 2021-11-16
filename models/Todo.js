a4dec5db63fb0cb777c9fd141d47e74f185c81cd
Danielle <43801670+DaniDoesDev@users.noreply.github.com>
2021-11-16 11:45:04 -0600

    initial Mongo server setup

---
+++ models/Todo.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const TodoSchema = new Schema(
+  {
+    title: {type: String, required: true},
+    description: {type: String, required: false,},
+    dateCreated: {type: String, required: true},
+    completed: {type: Boolean, required: true},
+    dateCompleted: {type: String, required: false},
+    author: {type: Schema.Types.ObjectId, ref: 'User'}
+  }
+);
+
+//Export model
+module.exports = mongoose.model('Todo', TodoSchema);
+
