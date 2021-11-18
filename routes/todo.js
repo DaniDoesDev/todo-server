014e5b4f6c6e7d933f8c69229bda5d03cfed4e81
Danielle <43801670+DaniDoesDev@users.noreply.github.com>
2021-11-17 18:16:42 -0600

    add uesr checks for toggle/delete

--- routes/todo.js
+++ routes/todo.js
@@ -5,64 +5,78 @@ const Todo = require('../models/Todo')
 
 const privateKey = process.env.JWT_PRIVATE_KEY;
 
-router.get('/', async function(req, res, next) {
+router.get('/', async function (req, res, next) {
     const todos = await Todo.find().exec()
-    return res.status(200).json({"todos": todos})
+    return res.status(200).json({ "todos": todos })
 });
 
-router.get('/:authorId', async function(req, res, next) {
+router.get('/:authorId', async function (req, res, next) {
     const todos = await Todo.find().where('author').equals(req.params.authorId).exec()
     console.log("CHECKED TODOS BY AUTHOR")
-    return res.status(200).json({"todos": todos})
+    return res.status(200).json({ "todos": todos })
 });
 
 // Validate before moving on
-router.use(function(req, res, next) {
-      console.log(req.header("Authorization"))
-      if (req.header("Authorization")) {
-          try {
-              req.payload = jwt.verify(req.header("Authorization"), privateKey, { algorithms: ['RS256'] })
-              console.log(req.payload)
-          } catch(error) {
-              return res.status(401).json({"error": error.message});
-          }
-      } else {
-          return res.status(401).json({"error": "Unauthorized"});
-      }
-      next()
-  })
+router.use(function (req, res, next) {
+    console.log(req.header("Authorization"))
+    if (req.header("Authorization")) {
+        try {
+            req.payload = jwt.verify(req.header("Authorization"), privateKey, { algorithms: ['RS256'] })
+            console.log(req.payload)
+        } catch (error) {
+            return res.status(401).json({ "error": error.message });
+        }
+    } else {
+        return res.status(401).json({ "error": "Unauthorized" });
+    }
+    next()
+})
 
 // router.get('/', async function(req, res, next) {
 //     const todos = await Todo.find().exec()
 //     return res.status(200).json({"todos": todos})
 // });
 
-router.delete('/:id', async function(req, res, next) {
-    const todo = await Todo.findByIdAndRemove(req.params.id).exec()
-    console.log("GOT A DELETE REQ")
-    return res.status(200).json(todo)
+router.delete('/:id', async function (req, res, next) {
+    if (req.body.username && req.body.author) {
+        if (req.body.username === req.body.author) {
+            const todo = await Todo.findByIdAndRemove(req.params.id).exec()
+            console.log("GOT A DELETE REQ")
+            return res.status(200).json(todo)
+        }
+        res.status(400).json({"error": "Not authorized to delete this post"})
+    } else {
+        res.status(400).json({"error": "Missing username or author in req body"})
+    }
 });
 
-router.patch('/:id', async function(req, res, next) {
-    const todo = await Todo.findByIdAndUpdate(req.params.id, {
-        completed: req.body.completed,
-        dateCompleted: req.body.dateCompleted
-    }).exec()
-    console.log("GOT A TOGGLE REQ")
-    return res.status(200).json(todo)
+router.patch('/:id', async function (req, res, next) {
+    if (req.body.username && req.body.author) {
+        if (req.body.username === req.body.author) {
+            const todo = await Todo.findByIdAndUpdate(req.params.id, {
+                completed: req.body.completed,
+                dateCompleted: req.body.dateCompleted
+            }).exec()
+            console.log("GOT A TOGGLE REQ")
+            return res.status(200).json(todo)
+        }
+        res.status(400).json({"error": "Not authorized to toggle this post"})
+    } else {
+        res.status(400).json({"error": "Missing username or author in req body"})
+    }
 });
 
 router.post('/', async function (req, res) {
-  const todo = new Todo({
-    "title": req.body.title,
-    "description": req.body.description,
-    "dateCreated": req.body.dateCreated,
-    "completed": req.body.completed,
-    "dateCompleted": req.body.dateCompleted,
-    "author": req.body.author
+    const todo = new Todo({
+        "title": req.body.title,
+        "description": req.body.description,
+        "dateCreated": req.body.dateCreated,
+        "completed": req.body.completed,
+        "dateCompleted": req.body.dateCompleted,
+        "author": req.body.author
     })
 
-    await todo.save().then( savedTodo => {
+    await todo.save().then(savedTodo => {
         return res.status(201).json({
             "id": savedTodo._id,
             "title": savedTodo.title,
@@ -72,8 +86,8 @@ router.post('/', async function (req, res) {
             "dateCompleted": savedTodo.dateCompleted,
             "author": savedTodo.author
         })
-    }).catch( error => {
-        return res.status(500).json({"error": error.message})
+    }).catch(error => {
+        return res.status(500).json({ "error": error.message })
     });
 })
 
